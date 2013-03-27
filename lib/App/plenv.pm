a406849f5378e6228b940aa29914626ac6967d01
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2013-03-27 06:15:18 +0000

    Import of TOKUHIROM/plenv-1.4.5 from CPAN.
    gitpan-cpan-distribution: plenv
    gitpan-cpan-version:      1.4.5
    gitpan-cpan-path:         TOKUHIROM/plenv-1.4.5.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

---
+++ lib/App/plenv.pm
@@ -0,0 +1,37 @@
+package App::plenv;
+use strict;
+use warnings;
+use 5.008002;
+our $VERSION = '1.4.5';
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+App::plenv - perl binary manager
+
+=head1 DESCRIPTION
+
+See L<plenv>
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+L<App::perlbrew> provides same feature. But plenv provides project local file: B< .perl-version >.
+
+Most of part was inspired from L<rbenv|https://github.com/sstephenson/rbenv>.
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
